eec244cb989a052d9df40c5f66934ae20cbeb349
Jorge Apaza <yochiwarez@gmail.com>
2019-12-11 22:27:52 -0500

    cleaning

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
-/* eslint-disable import/no-named-as-default-member */
 import Vue from 'vue';
 import axios from 'axios';
-
 import Autocomplete from '@trevoreyre/autocomplete-vue';
-// eslint-disable-next-line import/no-named-as-default
-import router from './vue/router/index';
-
 import main from './vue/views/main.vue';
-
-
 import '@trevoreyre/autocomplete-vue/dist/style.css';
-
 import './css/style.css';
 
-
 Vue.component('Autocomplete', Autocomplete);
-
 Vue.prototype.$http = axios;
 
-
 new Vue({
-  router,
+  render: (h) => h(main),
 }).$mount('#app');
